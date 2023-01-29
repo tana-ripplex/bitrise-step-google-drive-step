8a2da1e321e50d15e614e57eae3c588bbc4a09f6
Hiroko Tanaka <tanaka@ripplex.com>
2023-01-29 16:25:48 +0900

    main処理を追加

--- main.go
+++ main.go
@@ -1,31 +1,72 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
-	"os/exec"
+	"path/filepath"
+
+	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/googleapi"
 )
 
 func main() {
 	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
 
-	//
-	// --- Step Outputs: Export Environment Variables for other Steps:
-	// You can export Environment Variables for other Steps with
-	//  envman, which is automatically installed by `bitrise setup`.
-	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
-	if err != nil {
-		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
-		os.Exit(1)
-	}
-	// You can find more usage examples on envman's GitHub page
-	//  at: https://github.com/bitrise-io/envman
+	ctx := context.Background()
 
-	//
-	// --- Exit codes:
-	// The exit code of your Step is very important. If you return
-	//  with a 0 exit code `bitrise` will register your Step as "successful".
-	// Any non zero exit code will be registered as "failed" by `bitrise`.
+	srv, err := drive.NewService(ctx)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Drive client: %v", err)
+	}
+	filename := os.Getenv("deploy_item")
+	basename := filepath.Base(filename)
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	stat, err := file.Stat()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	folderid := os.Getenv("target_folder_id")
+
+	res, err := srv.Files.Create(
+		&drive.File{
+			Parents: []string{folderid},
+			Name:    basename,
+		},
+	).Media(file, googleapi.ChunkSize(int(stat.Size()))).Do()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("%s\n", res.Id)
+
+	res2, err := srv.Permissions.Create(res.Id, &drive.Permission{
+		Role: "reader",
+		Type: "anyone",
+	}).Do()
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%s\n", res2.DisplayName)
+
+	r, err := srv.Files.List().SupportsAllDrives(true).PageSize(1000).
+		Fields("files(id, name)").
+		Q(fmt.Sprintf("'%s' in parents", folderid)). // 特定のフォルダ配下
+		Context(ctx).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve files: %v", err)
+	}
+
+	for _, f := range r.Files {
+		println(f.Name, f.Id)
+	}
+
 	os.Exit(0)
 }
